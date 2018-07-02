81a5c8dc304cabd70db49f67b634a0b12ddac721
Shine <1075633455@qq.com>
2018-07-02 16:47:45 +0800

    添加头部，底部，活动页面

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -4,24 +4,18 @@ import { Layout } from 'antd';
 import styled from 'styled-components';
 import { HomeStore } from './Home.model';
 import Footer from '../components/footer/Footer';
+import Top from '../components/top/Top';
 
-const LayoutUI = styled(Layout)`
+const Homebox = styled.div`
 	position: relative;
 	min-height: 100vh;
-  .header {
-    text-align: center;
-    color: #fff;
-  }
-  .ant-layout-content {
-    min-width: 600px;
-    margin: 0 auto;
-  }
-  .ant-layout-footer{
-	  position: absolute;
-	  width: 100%;
-	  padding: 0;
-	  bottom: 0;
-  }
+	.main{
+		min-height: 100%;
+		>div{
+			width: 60%;
+			margin: 0 auto;
+		}
+	}
 `;
 
 @(connect(({ home }: any) => ({ home })) as any) // tslint:disable-line
@@ -37,44 +31,37 @@ export default class Home extends React.PureComponent<HomeProps, HomeState> {
 		dispatch({ type: 'home/query' });
 	}
 
-
 	render() {
 		return (
-			<LayoutUI>
-				<Layout.Header className="header">
-					Welcome to joys-react-scripts
-        		</Layout.Header>
-				<Layout>
-					<Layout.Content>
-						<div>
-							<h1>CSS 书写顺序</h1>
-							<p>
-								位置(position, top, right, bottom, left, z-index)<br />
-								尺寸(display，width, height, padding, margin, overflow)<br />
-								文字(font, line-height, letter-spacing, color, text-align)<br />
-								视觉(cursor, background, border, content, opacity, transform)<br />
-								动画(animation, transition)<br />
-								<a href="https://github.com/twitter/recess/blob/master/lib/lint/strict-property-order.js#L36">
-									完整顺序 (twitter)
-                				</a>
-							</p>
-						</div>
-						<div>
-							<h1>日志格式</h1>
-							<p>
-								[F] Fixed #2245: 修复一项 BUG<br />
-								[A] Feature #1190: new feature added. 添加了一项新功能<br />
-								[A] Added #2108: same as feature. 添加了一项新功能<br />
-								[R] Removed #985: 移除<br />
-								[I] Improved #186: 改进/提升<br />
-							</p>
-						</div>
-					</Layout.Content>
-				</Layout>
-				<Layout.Footer>
-					<Footer />
-				</Layout.Footer>
-			</LayoutUI>
+			<Homebox>
+				<Top />
+				<div className="main">
+					<div>
+						<h1>CSS 书写顺序</h1>
+						<p>
+							位置(position, top, right, bottom, left, z-index)<br />
+							尺寸(display，width, height, padding, margin, overflow)<br />
+							文字(font, line-height, letter-spacing, color, text-align)<br />
+							视觉(cursor, background, border, content, opacity, transform)<br />
+							动画(animation, transition)<br />
+							<a href="https://github.com/twitter/recess/blob/master/lib/lint/strict-property-order.js#L36">
+								完整顺序 (twitter)
+							</a>
+						</p>
+					</div>
+					<div>
+						<h1>日志格式</h1>
+						<p>
+							[F] Fixed #2245: 修复一项 BUG<br />
+							[A] Feature #1190: new feature added. 添加了一项新功能<br />
+							[A] Added #2108: same as feature. 添加了一项新功能<br />
+							[R] Removed #985: 移除<br />
+							[I] Improved #186: 改进/提升<br />
+						</p>
+					</div>
+				</div>
+				<Footer />
+			</Homebox>
 		);
 	}
 }
